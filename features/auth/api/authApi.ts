425a453e52804d0703b70743ed5dfd4a22c0b9c0
Vladimir Vasyliev <wvladimirw0502@gmail.com>
2025-07-20 21:52:57 +0300

    feat: add logic for expired link, fix form width, add alternative case #1 and #4

--- features/auth/api/authApi.ts
+++ features/auth/api/authApi.ts
@@ -5,16 +5,24 @@ import { Inputs } from "@/features/auth/ui/signUp/lib/schemas/signUp";
 export const authApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     registration: build.mutation({
-      query: async (body: Inputs) => {
-        await client.POST("/auth/registration", { body });
+      queryFn: async (body: Inputs) => {
+        const res = await client.POST("/auth/registration", { body });
+        return { data: res }
       },
     }),
     confirmEmail: build.mutation({
-      query: async (code: string) => {
-        await client.POST("/auth/confirm-email", { body: { code } });
+      queryFn: async (code: string) => {
+        const res = await client.POST("/auth/confirm-email", { body: { code } });
+        return { data: res }
+      },
+    }),
+    expiredLink: build.mutation({
+      queryFn: async (email: string) => {
+        const res = await client.POST("/auth/email-resend", { body: { email } });
+        return { data: res }
       },
     }),
   }),
 });
 
-export const { useRegistrationMutation, useConfirmEmailMutation } = authApi;
+export const { useRegistrationMutation, useConfirmEmailMutation, useExpiredLinkMutation } = authApi;
--- src/app/(auth)/confirm-registration/page.tsx
+++ src/app/(auth)/confirm-registration/page.tsx
@@ -1,23 +1,32 @@
-"use client";
+'use client';
 
-import s from "@/src/app/(auth)/confirm-registration/congratulations.module.scss";
-import { Button, UniversalIcon } from "@irondragons/ui-lib-inctagram";
-import { EmailConfirmationPage } from "features/auth/ui/emailConfirmationPage";
-import { redirect, useParams, useSearchParams } from "next/navigation";
-import { useConfirmEmailMutation } from "@/features/auth/api/authApi";
-import { useEffect } from "react";
+import s from '@/src/app/(auth)/confirm-registration/congratulations.module.scss';
+import { Button, UniversalIcon } from '@irondragons/ui-lib-inctagram';
+import { EmailConfirmationPage } from 'features/auth/ui/emailConfirmationPage';
+import { redirect, useParams, useSearchParams } from 'next/navigation';
+import { useConfirmEmailMutation } from '@/features/auth/api/authApi';
+import { useEffect } from 'react';
 
 const Page = () => {
   const [confirmEmailHandler] = useConfirmEmailMutation();
   const queryParams = useSearchParams();
-  const confirmationCode = queryParams.get("code");
-
+  const confirmationCode = queryParams.get('code');
+  
   // TODO: при разбиении кода, на бэке, сделать обработчик ошибок и редирект
-
+  
   useEffect(() => {
-    confirmEmailHandler(confirmationCode!).unwrap().then(console.log);
+    confirmEmailHandler(confirmationCode!).unwrap().then(res => {
+      const errorMessage = res.error?.errorsMessages[0]?.message
+      if(errorMessage === 'Confirmation code is expired' || errorMessage === 'Invalid confirmation code') {
+        redirect('/expired-link')
+      } else if (errorMessage === 'Email is already confirmed') {
+        redirect('/sign-in')
+      } else if (errorMessage === 'Invalid confirmation code') {
+        redirect('/sign-up')
+      }
+    });
   }, []);
-
+  
   return (
     <EmailConfirmationPage
       title="Congratulations!"
@@ -25,12 +34,12 @@ const Page = () => {
     >
       <>
         <div className={s.wrapper}>
-          <Button variant={"primary"} onClick={() => redirect("/sign-in")}>
+          <Button variant={'primary'} onClick={() => redirect('/sign-in')}>
             Sign In
           </Button>
         </div>
         <div className={s.iconWrapper}>
-          <UniversalIcon name={"Congratulations"} dataStatic={true} />
+          <UniversalIcon name={'Congratulations'} dataStatic={true} />
         </div>
       </>
     </EmailConfirmationPage>
