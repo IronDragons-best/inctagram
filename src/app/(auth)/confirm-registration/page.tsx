5863283ba0429bdbd6e5df52a8f2c685e3a4339b
VladosDeveloper <yurenya.vlad.01@mail.ru>
2025-07-21 10:48:35 +0300

    fix: bug with loader

--- src/app/(auth)/confirm-registration/page.tsx
+++ src/app/(auth)/confirm-registration/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import s from "@/src/app/(auth)/confirm-registration/congratulations.module.scss";
 import { Button, UniversalIcon } from "@irondragons/ui-lib-inctagram";
@@ -6,7 +6,8 @@ import { EmailConfirmationPage } from "features/auth/ui/emailConfirmationPage";
 import { redirect, useParams, useSearchParams } from "next/navigation";
 import { useConfirmEmailMutation } from "@/features/auth/api/authApi";
 import { useEffect, useState } from "react";
-import { Ring } from 'ldrs/react';
+import { Ring } from "ldrs/react";
+import "ldrs/react/Ring.css";
 
 const Page = () => {
   const [isEmailConfirmed, setIsEmailConfirmed] = useState(false);
@@ -15,6 +16,7 @@ const Page = () => {
   const confirmationCode = queryParams.get("code");
 
   useEffect(() => {
+    if (isLoading) return;
     setIsEmailConfirmed(true);
     confirmEmailHandler(confirmationCode!)
       .unwrap()
@@ -44,12 +46,12 @@ const Page = () => {
     >
       <>
         <div className={s.wrapper}>
-          <Button variant={'primary'} onClick={() => redirect('/sign-in')}>
+          <Button variant={"primary"} onClick={() => redirect("/sign-in")}>
             Sign In
           </Button>
         </div>
         <div className={s.iconWrapper}>
-          <UniversalIcon name={'Congratulations'} dataStatic={true} />
+          <UniversalIcon name={"Congratulations"} dataStatic={true} />
         </div>
       </>
     </EmailConfirmationPage>
