f5deca863bc01f7066212fc46b850caca3385038
VladosDeveloper <yurenya.vlad.01@mail.ru>
2025-08-03 17:12:19 +0300

    feat: add a little logic for me request

--- features/auth/api/authApi.ts
+++ features/auth/api/authApi.ts
@@ -1,25 +1,27 @@
-import { client } from '@/shared/schemas/types/api/client';
-import { baseApi } from '@/src/app/provider/baseApi';
-import { Inputs } from '@/views/auth/pages/signUp/lib/schemas/signUp';
-import { InputsForm } from '@/views/auth/pages/signIn/lib/schemas/signIn';
-import { InputForm } from '@/views/auth/pages/forgot-password/lib/schemas/forgotPasswordForm';
+import { client, TokenService } from "@/shared/schemas/types/api/client";
+import { baseApi } from "@/src/app/provider/baseApi";
+import { Inputs } from "@/views/auth/pages/signUp/lib/schemas/signUp";
+import { InputsForm } from "@/views/auth/pages/signIn/lib/schemas/signIn";
+import { InputForm } from "@/views/auth/pages/forgot-password/lib/schemas/forgotPasswordForm";
+import { Mutex } from "async-mutex";
 
+const mutex = new Mutex();
 
 export const authApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     registration: build.mutation({
       queryFn: async (body: Inputs) => {
-        const res = await client.POST('/auth/registration', { body });
+        const res = await client.POST("/auth/registration", { body });
         return { data: res };
       },
     }),
     confirmEmail: build.mutation({
       queryFn: async (code: string) => {
         try {
-          const res = await client.POST('/auth/confirm-email', {
+          const res = await client.POST("/auth/confirm-email", {
             body: { code },
           });
-          
+
           if (res.error) {
             return {
               error: {
@@ -30,13 +32,12 @@ export const authApi = baseApi.injectEndpoints({
           }
 
           return { data: res.response.status };
-
         } catch (e) {
           return {
             error: {
               status: 500,
               data: {
-                message: 'Unknown error occurred',
+                message: "Unknown error occurred",
                 details: e instanceof Error ? e.message : String(e),
               },
             },
@@ -46,7 +47,7 @@ export const authApi = baseApi.injectEndpoints({
     }),
     expiredLink: build.mutation({
       queryFn: async (email: string) => {
-        const res = await client.POST('/auth/email-resend', {
+        const res = await client.POST("/auth/email-resend", {
           body: { email },
         });
         return { data: res };
@@ -54,32 +55,58 @@ export const authApi = baseApi.injectEndpoints({
     }),
     signIn: build.mutation({
       queryFn: async (body: InputsForm) => {
-        const res = await client.POST('/auth/login', { body });
+        const res = await client.POST("/auth/login", { body });
         return { data: res };
       },
     }),
     logout: build.mutation({
       queryFn: async () => {
-        const res = await client.POST('/auth/logout');
+        const res = await client.POST("/auth/logout");
         return { data: res };
       },
     }),
     me: build.query<any, void>({
       queryFn: async () => {
-        const res = await client.GET('/auth/me');
+        const res = await client.GET("/auth/me");
         return { data: res };
+      },
+      async onQueryStarted(arg, { dispatch, getState, queryFulfilled }) {
+        const release = await mutex.acquire();
+        try {
+          const { data } = await queryFulfilled;
+          debugger;
+          if (data.response.status === 401) {
+            const { data: token } = await client.POST("/auth/refresh-token");
+            debugger;
+            if (token) {
+              TokenService.setToken(token.accessToken);
+              const res = await client.GET("/auth/me");
+              const { data } = await queryFulfilled;
+            }
+          }
+        } finally {
+          release();
+        }
       },
     }),
     refreshToken: build.mutation<any, void>({
       queryFn: async () => {
-        const res = await client.POST('/auth/refresh-token');
+        const res = await client.POST("/auth/refresh-token");
         return { data: res };
+      },
+      async onQueryStarted(arg, { dispatch, getState, queryFulfilled }) {
+        const release = await mutex.acquire();
+        try {
+          await queryFulfilled;
+        } finally {
+          release();
+        }
       },
     }),
     reCaptcha: build.mutation({
       queryFn: async (data: InputForm) => {
-        const res = await client.POST('/auth/password-recovery', {
-          body: data ,
+        const res = await client.POST("/auth/password-recovery", {
+          body: data,
         });
         return { data: res };
       },
