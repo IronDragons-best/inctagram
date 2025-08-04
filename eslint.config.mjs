fc1cd3335633dfec4f78a648b907dc09210854c4
VladosDeveloper <yurenya.vlad.01@mail.ru>
2025-08-04 20:18:50 +0300

    feat: add linters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,41 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pkg from '@next/eslint-plugin-next';
+import unusedImports from 'eslint-plugin-unused-imports'
+const { configs } = pkg;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: configs.recommended
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   
+  {
+    plugins: {
+      'unused-imports': unusedImports
+    },
+    rules: {
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'space-before-blocks': ['error', 'always'],
+      'keyword-spacing': ['error', { before: true, after: true }],
+      
+      'unused-imports/no-unused-imports': 'warn',
+      'no-unused-vars': ['warn', {
+        vars: 'all', // следить за всеми переменными
+        args: 'after-used', // параметры функций после последнего использованного
+        ignoreRestSiblings: true, // полезно с rest-операторами
+        argsIgnorePattern: '^_', // игнорировать параметры, начинающиеся с "_"
+        varsIgnorePattern: '^_'  // игнорировать переменные, начинающиеся с "_"
+      }]
+    }
+  }
 ];
 
 export default eslintConfig;
