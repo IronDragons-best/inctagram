2dee9da4b867ab4b1fa9b4edb81b550bca286e36
VladosDeveloper <yurenya.vlad.01@mail.ru>
2025-08-06 21:46:14 +0300

    feat: add linters packages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 // middleware.ts
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { PATH } from "@/shared/constants/path";
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
   // Логирование пути
