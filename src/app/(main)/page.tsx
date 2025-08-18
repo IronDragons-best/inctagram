41c7509c6cb1343009ee88bb9768681771644637
Nikita Vlasov <nikistosov@gmail.com>
2025-08-18 10:43:54 +0200

    fix: critikal bug
    - hardcode Post

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,9 @@
 import { PublicPage } from 'views/publicPage/pages/public-page/ui'
-import { Post } from '@/views/profile/pages/userProfile/userPost/post'
 
 export default function Home() {
   return (
     <>
       <PublicPage />
-      <Post isModalOpen={true} srcArray={[]} />
     </>
   )
 }
