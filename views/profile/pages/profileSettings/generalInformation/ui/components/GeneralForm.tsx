66a7759448ae8a94be9250f804f7c1a92e620c67
Vladimir Vasyliev <wvladimirw0502@gmail.com>
2025-08-25 12:51:40 +0300

    fix: bugs DatePicker

--- views/profile/pages/profileSettings/generalInformation/ui/components/GeneralForm.tsx
+++ views/profile/pages/profileSettings/generalInformation/ui/components/GeneralForm.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { DatePicker, Input, Selectbox, TextAreaComponent } from '@irondragons/ui-lib-inctagram'
-import { useState } from 'react'
-import { DateRange } from 'react-day-picker'
+import { Input, Selectbox, TextAreaComponent } from '@irondragons/ui-lib-inctagram'
 import { useFormContext } from 'react-hook-form'
 import { InputsName } from '../../lib/schema'
 import s from './components.module.scss'
+import { DatePicker } from '@/views/datePicker/DatePicker'
+import { useState } from 'react'
+import { DateRange } from 'react-day-picker'
 
 export const GeneralForm = () => {
   const {
