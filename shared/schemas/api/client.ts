7f038b20fe56e0280a03b8f78429919f778e0948
VladosDeveloper <yurenya.vlad.01@mail.ru>
2025-09-14 21:54:02 +0300

    fix: for prod

--- shared/schemas/api/client.ts
+++ shared/schemas/api/client.ts
@@ -1,9 +1,10 @@
 import createClient from 'openapi-fetch'
 import { paths } from './schema'
+import * as process from 'node:process'
 
 export const getClient = () => {
   return createClient<paths>({
-    baseUrl: 'https://nodewebdev.online/api/v1',
+    baseUrl: process.env.PUBLIC_BASE_URL,
     headers: {},
     credentials: 'include',
   })
